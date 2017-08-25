e4349cce67dc15c9590235b0ca1827f72efc26f2
Jose <jomoespe@gmail.com>
2017-08-25 22:11:08 +0200

    Add custom events support
    The web component now fires titleChanged event, and the index.html page add a listener
    to this event and write to console.
    
    Following this article: https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Creating_and_triggering_events

--- html/example1/js/index.js
+++ html/example1/js/index.js
@@ -3,9 +3,15 @@
  */
 document.addEventListener("DOMContentLoaded", e => {
 	const title = document.querySelector("#titlePanel");
-	document.querySelector("button").addEventListener("click", e => {
-		title.title = "Your orders";
-		title.subtitle = "Here you can see your orders, current orders state, etc";
-		title.image = "images/teaser-order.png";
-	});
+
+    document.querySelector("button").addEventListener("click", e => {
+        title.title = "Your orders";
+        title.subtitle = "Here you can see your orders, current orders state, etc";
+        title.image = "images/teaser-order.png";
+    });
+
+    // registering to a web component custom event 
+    title.addEventListener('titleChanged', event => {
+        console.log('titleChanged: new title=' + event.detail.title)
+    }, false);
 }, true);
