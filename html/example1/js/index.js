a176e86c63d0a299e5c198a32d4d40ab9d473b3c
Jose <jomoespe@gmail.com>
2017-08-27 11:46:29 +0200

    Cleanup and reorder code

--- html/example1/js/index.js
+++ html/example1/js/index.js
@@ -2,16 +2,18 @@
  * This is the script for index.html page
  */
 document.addEventListener("DOMContentLoaded", e => {
-	const title = document.querySelector("#titlePanel");
+    const title  = document.querySelector("#titlePanel");
+    const button = document.querySelector("button");
     // registering to a web component custom event 
     title.addEventListener('attributeChanged', event => {
-        console.log('attributeChanged: attribute=[', event.detail.attr, '], old value=[', event.detail.oldValue, "], new value=[", event.detail.newValue, "]")
+        console.log('attributeChanged: attribute=', event.detail.attr,
+                                   ', old value=', event.detail.oldValue, 
+                                   ', new value=', event.detail.newValue)
     });
-
-    document.querySelector("button").addEventListener("click", e => {
-        title.title = "Your orders";
+    button.addEventListener("click", e => {
+        title.title    = "Your orders";
         title.subtitle = "Here you can see your orders, current orders state, etc";
-        title.image = "images/teaser-order.png";
+        title.image    = "images/teaser-order.png";
     });
 }, true);
     
