aa948af7d01f894e4204ad46f5018455b6937a1b
Jose <jomoespe@gmail.com>
2017-08-27 11:29:54 +0200

    Change titleChanged event to propertyChanged event
    This style allows a more standart generic way to handle web component attribute events.

--- html/example1/js/index.js
+++ html/example1/js/index.js
@@ -3,15 +3,15 @@
  */
 document.addEventListener("DOMContentLoaded", e => {
 	const title = document.querySelector("#titlePanel");
+    // registering to a web component custom event 
+    title.addEventListener('attributeChanged', event => {
+        console.log('attributeChanged: attribute=[', event.detail.attr, '], old value=[', event.detail.oldValue, "], new value=[", event.detail.newValue, "]")
+    });
 
     document.querySelector("button").addEventListener("click", e => {
         title.title = "Your orders";
         title.subtitle = "Here you can see your orders, current orders state, etc";
         title.image = "images/teaser-order.png";
     });
-
-    // registering to a web component custom event 
-    title.addEventListener('titleChanged', event => {
-        console.log('titleChanged: new title=' + event.detail.title)
-    }, false);
 }, true);
+    
