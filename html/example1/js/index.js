8ac26a17b184b65d5657b0476c9033a7bfbf009b
Jose <jomoespe@gmail.com>
2017-10-09 23:35:47 +0200

    Add button to example1
    Added a second button to change title panel values

--- html/example1/js/index.js
+++ html/example1/js/index.js
@@ -3,17 +3,23 @@
  */
 document.addEventListener("DOMContentLoaded", e => {
     const title  = document.querySelector("#titlePanel");
-    const button = document.querySelector("button");
+    const button1 = document.querySelector("#button-1");
+    const button2 = document.querySelector("#button-2");
     // registering to a web component custom event 
     title.addEventListener('attributeChanged', event => {
         console.log('attributeChanged: attribute=', event.detail.attr,
                                    ', old value=', event.detail.oldValue, 
                                    ', new value=', event.detail.newValue)
     });
-    button.addEventListener("click", e => {
+    button1.addEventListener("click", e => {
         title.title    = "Your orders";
         title.subtitle = "Here you can see your orders, current orders state, etc";
         title.image    = "images/teaser-order.png";
+    });
+    button2.addEventListener("click", e => {
+        title.title    = "Other title";
+        title.subtitle = "Here other title";
+        title.image    = "images/title-image.png";
     });
 }, true);
     
